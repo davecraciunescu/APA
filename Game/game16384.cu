7efa2547acbade1388113598fa4c2e0a994d2f24
PabloAceG <pabloacereda98@gmail.com>
2019-03-10 21:23:29 +0100

    Metodo para mostrar grid que recibe filas, columnas y la matrix a representar. Metodo de transferencia de datos CPO -> GPU     & Falta configurar el tamaño del kernel

--- Game/game16384.cu
+++ Game/game16384.cu
@@ -1,3 +1,6 @@
+// -----------------------------------------------------------------------------
+// ------------------------------ LIBRARIES ------------------------------------
+// -----------------------------------------------------------------------------
 // Includes CUDA
 #include <cuda_runtime.h>
 // CUDA get characteristics
@@ -6,6 +9,13 @@
 #include <string>
 // C++ library for I/O 
 #include <iostream>
+// Calloc, exit, free
+#include <stdlib.h>
+
+// -----------------------------------------------------------------------------
+// ------------------------------- HEADERS -------------------------------------
+// -----------------------------------------------------------------------------
+cudaError_t sendMatrixToGpu(int row, int column, int* matrix);
 
 // Method which allows to check for errors
 __host__ void check_CUDA_Error(const char *msg)
@@ -39,11 +49,11 @@ __host__ int getThreadsBlock()
     cudaGetDeviceCount(&nDevices);
     check_CUDA_Error("Couldn't get the number of devices in this computer");
 
-    for(int i = 0; i < nDevices; i++)
+    if(cnDevices > 0)
     {
         cudaDeviceProp prop;
         
-        cudaGetDeviceProperties(&prop, i);
+        cudaGetDeviceProperties(&prop, 0);
         
         threadsBlock = prop.maxThreadsPerBlock;
     }
@@ -51,6 +61,41 @@ __host__ int getThreadsBlock()
     return threadsBlock;
 }
 
+// -----------------------------------------------------------------------------
+// ------------------------------ GAME METHODS ---------------------------------
+// -----------------------------------------------------------------------------
+void displayGrid(int rows, int columns, int* Matrix)
+{
+    system("clear");
+   
+    std::cout << std::endl;
+
+    for(int i = 0; i < rows; i++)
+    {
+        std::cout << "| ";
+
+        for(int j = 0; j < columns; j++)
+        {
+            std::cout << Matrix[i * rows + j] << " | ";
+        }
+
+        std::cout << std::endl << std::endl;
+    }
+
+    std::cout <<                          std::endl 
+              <<                          std::endl 
+              << "Controls: "          << std::endl
+              << "         _"          << std::endl
+              << "       | W |"        << std::endl
+              << "  _      _      _"   << std::endl
+              << "| A |  | S |  | D |" << std::endl
+              <<                          std::endl;
+}
+
+
+// -----------------------------------------------------------------------------
+// -------------------------------- MAIN CODE ----------------------------------
+// -----------------------------------------------------------------------------
 int main(int argc, char** argv)
 {
     // Game Mode
@@ -154,6 +199,58 @@ int main(int argc, char** argv)
                       << "The maximum number of cells is " << numMaxThreads
                       << std::endl;
         }
+    
+        int* Matrix = (int*) calloc(numRows * numColumns, sizeof(int));
+
+        displayGrid(numRows, numColumns, Matrix);        
+
     }
 }
+
+// -----------------------------------------------------------------------------
+// ----------------------------- CUDA METHODS-----------------------------------
+// -----------------------------------------------------------------------------
+sendMatrixToGpu(int row, int column, int* matrix)
+{
+    // Variable to operate in the GPU
+    int* dev_matrix = 0;
+    
+    // TODO: Set Kernel dimensions
+    // GPU threads distribution
+    dim3 dimGrid();
+    dim3 dimBlock();
+
+    // Selection of the GPU were code is to be executed
+    cudaSetDevice(0);
+    check_CUDA_Error("cudaSetDevice failed!\n");
+    
+    // Memory Allocation for GPU Variables
+    cudaMalloc((void**) &dev_matrix, row * column * sizeof(int));
+    check_CUDA_Error("cudaMalloc failed at Matrix!\n");
+
+    // Memory Transfer: CPU -> GPU
+    cudaMemcpy(dev_matrix, matrix, row * column * sizeof(int),
+               cudaMemcpyHostToDevice);
+    check_CUDA_Error("cudaMemCpy failed at Matrix!\n");
+
+    // TODO: Description
+    // TODO: Kernel
+
+    // TODO: Insert appropiate failed text for kernel
+    check_CUDA_Error("");
+
+
+    // Waits for kernel to finish
+    cudaDeviceSynchronize();
+    check_CUDA_Error("cudaDeviceSynchronize returned error!");
+
+    // Memory Transfer: GPU -> CPU
+    cudaMemcpy(matrix, dev_matrix, row * column * sizeof(int),
+               cudaMemcpyDeviceToHost);
+    check_CUDA_Error("cudaMemCpy failed after copying from GPU to CPU!\n");
+
+    return cudaGetLastError();
+}
+
+
 
